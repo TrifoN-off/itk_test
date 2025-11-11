d9b7516b46b1ff674bddc50394856d28976243c5
Nikita Trifonov <trifonov.nikit@gmail.com>
2025-11-12 01:55:39 +0300

    added schemas and wallet repo

---
+++ app/repositories/wallet_repositroy.py
@@ -0,0 +1,48 @@
+from typing import Optional
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models.wallet import Wallet
+from schemas.wallet import WalletCreate
+
+
+class WalletRepository:
+    """Репозиторий для работы с кошельками."""
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def create(self, wallet_data: WalletCreate) -> Wallet:
+        """Создать новый кошелек."""
+        db_wallet = Wallet(**wallet_data.model_dump())
+        self.db.add(db_wallet)
+        await self.db.commit()
+        await self.db.refresh(db_wallet)
+        return db_wallet
+
+    async def get_by_uuid(self, wallet_uuid: UUID) -> Optional[Wallet]:
+        """Получить кошелек по uuid."""
+        result = await self.db.execute(
+            select(Wallet).where(Wallet.id == wallet_uuid)
+        )
+        return result.scalars().first()
+
+    async def get_by_uuid_with_lock(
+        self, wallet_uuid: UUID
+    ) -> Optional[Wallet]:
+        """
+        Получить кошелек по uuid с блокировкой.
+        Необходимо для правильной обработки конкурентных операций.
+        """
+        result = await self.db.execute(
+            select(Wallet).where(Wallet.id == wallet_uuid).with_for_update()
+        )
+        return result.scalars().first()
+
+    async def update_balance(self, wallet: Wallet, new_balance: int) -> Wallet:
+        """Обновить баланс кошелька."""
+        wallet.balance = new_balance
+        await self.db.commit()
+        await self.db.refresh(wallet)
+        return wallet
---
+++ app/schemas/wallet.py
@@ -0,0 +1,34 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, Field, UUID4, ConfigDict
+
+from models.wallet import WALLET_BALANCE_DEFAULT
+
+
+class OperationType(str, Enum):
+    DEPOSIT = 'DEPOSIT'
+    WITHDRAW = 'WITHDRAW'
+
+
+class WalletOperationRequest(BaseModel):
+    operation_type: OperationType = Field(
+        ...,
+        description='Operation type'
+    )
+    amount: int = Field(
+        ...,
+        gt=0,
+        description='Operation amount'
+    )
+
+
+class WalletCreate(BaseModel):
+    balance: Optional[int] = Field(default=WALLET_BALANCE_DEFAULT)
+
+
+class WalletResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID4
+    balance: int
